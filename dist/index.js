62efec967c97d455d175012cc35a30cc551f62f2
Omkar Patil <git21cs072-t@students.git.edu>
2023-09-15 20:09:13 +0530

    ts project

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("hello world");
+let age = 22;
+console.log(age);
+if (age < 50)
+    age += 50;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("hello world");
+let age: number = 22;
+console.log(age);
+
+if (age < 50) age += 50;
