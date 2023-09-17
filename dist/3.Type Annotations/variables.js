8138280a5cc0e2cfb9c06d67707a267956e5df8f
Omkar Patil <git21cs072-t@students.git.edu>
2023-09-17 10:07:51 +0530

    "Type Annotations Basics"

---
+++ dist/3.Type Annotations/variables.js
@@ -0,0 +1,9 @@
+"use strict";
+let movie = "oppenheimer";
+let x = 99;
+let y = true;
+let u = "hiii";
+u = 45;
+u = false;
+let l = 123;
+//# sourceMappingURL=variables.js.map
---
+++ src/3.Type Annotations/variables.ts
@@ -0,0 +1,18 @@
+// some data type and declaring of variables
+
+let movie: string = "oppenheimer";
+
+let x: number = 99; // number type includes all float, integer , doubles
+let y: boolean = true; //boolean type
+
+// any type
+
+let u: any = "hiii";
+u = 45;
+u = false;
+
+//type inferencing
+
+let l = 123;
+//if we do now
+// l = 'hii'; //means typescript automatically detects the type of variable based upon its value <==
