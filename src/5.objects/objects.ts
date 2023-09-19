cd5d81b7193ef54915fe3b3ce65cf3d1fca8b88a
Omkar Patil <git21cs072-t@students.git.edu>
2023-09-19 22:13:55 +0530

    more object

--- src/5.objects/objects.ts
+++ src/5.objects/objects.ts
@@ -18,3 +18,41 @@ const p1 = {
   last: "Patil",
 };
 console.log(printName(p1));
+
+//more about object typess
+let cord: { x: number; y: number } = { x: 45, y: 56 };
+
+//function returning object
+function findCord(): { x: number; y: number } {
+  return { x: Math.random(), y: Math.random() };
+}
+
+//excess parameters trap
+
+//1. invalid case
+// printName({ first: "omkar", last: "patil", age: 22 }); //invalid
+
+//but
+const p2 = {
+  first: "Omkar",
+  last: "Patil",
+  age: 22,
+};
+printName(p2); //valid
+
+//type aliasing
+
+function pt(point: { x: number; y: number }): { x: number; y: number } {
+  return { x: point.x * 2, y: point.y * 4 };
+}
+
+//using aliasing
+type pointy = {
+  x: number;
+  y: number;
+};
+function pt1(point: pointy): pointy {
+  return { x: point.x * 2, y: point.y * 4 };
+}
+
+//both functions yeild same output
